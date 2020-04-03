b0c2520fc77e4fc17b24ba1ae52fe2d48d112e78
Rooshi Patidar <rooshipatidar@gmail.com>
2020-04-03 13:54:34 -0500

    link to firebase

--- app/src/main/java/me/rooshi/podcastapp/MainActivity.java
+++ app/src/main/java/me/rooshi/podcastapp/MainActivity.java
@@ -3,6 +3,7 @@ package me.rooshi.podcastapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void searchTerm(View view) {
+
     }
 }
