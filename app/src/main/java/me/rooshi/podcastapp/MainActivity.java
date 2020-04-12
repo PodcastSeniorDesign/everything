1cddc5a226310f02b344caab8998f71c412b4033
Rishab Chander <rchand20@utexas.edu>
2020-04-12 00:07:02 -0500

    Added facebook login capability from Rishab Button

--- app/src/main/java/me/rooshi/podcastapp/MainActivity.java
+++ app/src/main/java/me/rooshi/podcastapp/MainActivity.java
@@ -3,11 +3,15 @@ package me.rooshi.podcastapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,6 +21,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView testTextView = findViewById(R.id.testTextView);
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String baseItunesUrl ="https://itunes.apple.com/search?media=podcast";
+        String baseItunesUrl = "https://itunes.apple.com/search?media=podcast";
         Uri uri = Uri.parse(baseItunesUrl);
         uri = uri.buildUpon().appendQueryParameter("term", toSearch).build();
         String url = uri.toString();
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        testTextView.setText("Response is: "+ response);
+                        testTextView.setText("Response is: " + response);
                         FirebaseDatabase database = FirebaseDatabase.getInstance();
                         DatabaseReference myRef = database.getReference("search");
 
@@ -63,5 +70,10 @@ public class MainActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
 
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
     }
 }
