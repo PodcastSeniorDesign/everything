5b0a2772a1fcc7c4bbd9cda6ea75870df386b104
Kaila Prochaska <prochaska.kaila@gamil.com>
2020-04-12 01:13:41 -0700

    button click change text

--- app/src/main/java/me/rooshi/podcastapp/MainActivity.java
+++ app/src/main/java/me/rooshi/podcastapp/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DownloadManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ===== Kaila's button click =====
+        final Button kailabtn = (Button) findViewById(R.id.kaila);
+        kailabtn.setOnClickListener(new View.OnClickListener() { // we're passing in an instance of a new onclicklistener
+            @Override
+            public void onClick(View view) {
+//                Log.i("kaila_btn", "kaila's log message!!!! :)");
+//                Toast.makeText(getApplicationContext(), "clicked the button!", Toast.LENGTH_SHORT).show();//show a toast message
+                // context is give it a context that is the UI that is being shown
+                kailabtn.setText("clicked!");
+            }
+        });
+
+
     }
 
     public void searchTerm(View view) {
